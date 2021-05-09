4eb1226c0a55b65779be283cbd6e616da0b19a13
Ivan <iborisov@pryaniky.onmicrosoft.com>
2021-05-09 20:36:21 +0300

    finish

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# caesarCipher
+# caesar cipher RSS Task 1
+# how to use?
+
+## clone repo
+## npm i
+
+## commands
+-a, --action [type] action (encode or decode)
+-s --shift <number> shift
+-i --input <filename> input file path
+-o --output <filename> output file path
+
+# Example
+
+1) create input.txt file in root
+2) enter text
+3) for encode
+## node index.js -a encode -s 7 -i "./input.txt" -o "./output.txt"
+4) for decode
+## node index.js -a decode -s 7 -i "./output.txt" -o "./decode.txt"
---
+++ Transform.js
@@ -0,0 +1,43 @@
+const stream = require('stream');
+const {
+  CODE_SHIFT,
+  LOWER_CODES_END,
+  LOWER_CODES_START,
+  UPPER_CODES_END,
+  UPPER_CODES_START,
+  LAT_SYMBOLS_REGEX
+} = require('./constants');
+
+const doShift = (str, shift) => {
+  if (shift < 0) return doShift(str, shift + CODE_SHIFT);
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char.match(LAT_SYMBOLS_REGEX)) {
+      const code = str.charCodeAt(i);
+      if (code >= UPPER_CODES_START && code <= UPPER_CODES_END) {
+        char = String.fromCharCode(((code - UPPER_CODES_START + shift) % CODE_SHIFT) + UPPER_CODES_START);
+      } else if (code >= LOWER_CODES_START && code <= LOWER_CODES_END) {
+        char = String.fromCharCode(((code - LOWER_CODES_START + shift) % CODE_SHIFT) + LOWER_CODES_START);
+      }
+    }
+    result += char;
+  }
+  return result;
+};
+
+class Transformer extends stream.Transform {
+  constructor(action, shift) {
+    super();
+    this.shift = this._combine(action, shift);
+  }
+  _combine(action, shift) {
+    return action === 'encode' ? +shift : -shift;
+  }
+  _transform(data, _, callback) {
+    this.push(doShift(data.toString(), this.shift));
+    callback();
+  }
+};
+
+module.exports = Transformer;
